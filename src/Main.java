084406149923943fd14a948d7db764a671d9ad94
Francesc Constans <fconstans@lasalle.cat>
2024-10-21 08:39:18 +0200

    afegir dades noves

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        float[] scores = {7f, 2.5f,8.9f,10.0f,18.5f};
+        Arrays.sort(scores);
+        System.out.println("El array ordenado es: ");
+        for (int i=0; i<scores.length;i++){
+            System.out.print(scores[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
